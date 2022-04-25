6d96b55b1629cd2fe8bc39be2e68133a130e639b
sunghyeok <dodri4321@naver.com>
2022-04-25 13:00:32 +0900

    1

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,24 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private String name1;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
